076a7286fe3e679ce434919d2cb25ae0f41397a9
Buzov Vyahceslav <buzovv1997@gmail.com>
2020-12-14 13:41:05 +0300

    working search handles

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import json
 from os.path import join, dirname
 from os import listdir
 from random import choices
-
+from math import ceil
 from app.index import index, find
 
 app = Flask(__name__)
@@ -26,16 +26,33 @@ def get_json(cook_id):
     return data
 
 
-@app.route('/title_img/<int:cook_id>')
+@app.route('/title_img/<int:cook_id>', methods=['GET'])
 def get_title_img(cook_id):
     filename = join(IMG_DIR, str(cook_id), 'title.jpg')
     return send_file(filename, mimetype='image/jpg')
 
 
-@app.route('/step_img/<int:cook_id>/<img_name>')
+@app.route('/step_img/<int:cook_id>/<img_name>', methods=['GET'])
 def get_step_img(cook_id, img_name):
     filename = join(IMG_DIR, str(cook_id), img_name)
     return send_file(filename, mimetype='image/jpg')
+
+
+@app.route('/find/page_count/<substr>', methods=['GET'])
+@app.route('/find/page_count/<substr>/<int:items_on_page>', methods=['GET'])
+def get_page_count(substr, items_on_page=10):
+    result = find(substr)
+    page_count = ceil(len(result)/items_on_page)
+    return {'page_count': page_count}
+
+
+@app.route('/find/list/<substr>', methods=['GET'])
+@app.route('/find/list/<substr>/<int:page>', methods=['GET'])
+@app.route('/find/list/<substr>/<int:items_on_page>/<int:page>', methods=['GET'])
+def get_page_array(substr, items_on_page=10, page=1):
+    result = find(substr)
+    page_list = result[(page-1)*items_on_page:page*items_on_page]
+    return {"items": page_list}
 
 
 if __name__ == '__main__':
